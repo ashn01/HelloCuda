c726f94ad86bbf519b0977bda5399e9ff88ba625
YMK <ashn01@naver.com>
2024-04-16 19:09:00 -0400

    Hello CUDA

---
+++ HelloCuda/kernel.cu
@@ -0,0 +1,15 @@
+﻿#include <cstdio>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+
+__global__ void hello(void) {
+	printf("hello CUDA %d!\n", threadIdx.x);
+}
+
+int main(void) {
+	hello <<< 8, 2 >>> ();
+	fflush(stdout);
+	return 0;
+}
